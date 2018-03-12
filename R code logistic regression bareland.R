189cbb2c78567fcbb5db4185a15eeac7ff890add
Charles Wassell <wassellc@gmail.com>
2018-03-11 22:43:23 -0700

    Added partial effect code, plus code to examine 2011 impacts for those parcels that declined in 09.10

--- R code logistic regression bareland.R
+++ R code logistic regression bareland.R
@@ -12,3 +12,23 @@ summary(logit.2)
 
 exp( cbind ( OR = coef ( logit.2 ), confint ( logit.2 ) ) )
 
+library(mfx)
+
+logit.partial.average <- logitmfx( Drop_09.10 ~ YellowRed + TotalAcres + large.parcel, data = assessed_bare, atmean = TRUE )
+logit.partial.average
+
+logit.average.partial <- logitmfx( Drop_09.10 ~ YellowRed + TotalAcres + large.parcel, data = assessed_bare, atmean = FALSE )
+logit.average.partial
+
+
+assessed_bare$change.0910.to.1011 <- ( assessed_bare$AssessedLandValue_2011 - assessed_bare$AssessedLandValue_2010 ) / (assessed_bare$AssessedLandValue_2010) 
+assessed_bare$change.0910.to.1011
+
+qplot(assessed_bare$change.0910.to.1011)
+
+
+library(dplyr)
+
+declined.09.10 <- select ( filter ( assessed_bare, assessed_bare$Drop_09.10 == 1 ), c(change.0910.to.1011))
+declined.09.10
+summary(declined.09.10)
