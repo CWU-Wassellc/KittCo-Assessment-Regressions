bdef4d05271aa1e9643774bd32ba5a78891e8192
Charles Wassell <wassellc@gmail.com>
2018-03-11 23:21:26 -0700

    Added dummy var for dropped 10.11, plus dummy for drop in 09.10 or 10.11

--- R code logistic regression bareland.R
+++ R code logistic regression bareland.R
@@ -12,6 +12,9 @@ summary(logit.2)
 
 exp( cbind ( OR = coef ( logit.2 ), confint ( logit.2 ) ) )
 
+
+#############################################################################
+
 library(mfx)
 
 logit.partial.average <- logitmfx( Drop_09.10 ~ YellowRed + TotalAcres + large.parcel, data = assessed_bare, atmean = TRUE )
@@ -20,6 +23,17 @@ logit.partial.average
 logit.average.partial <- logitmfx( Drop_09.10 ~ YellowRed + TotalAcres + large.parcel, data = assessed_bare, atmean = FALSE )
 logit.average.partial
 
+### Dropped either 09_10 or 10_11
+
+logit.partial.average.0911 <- logitmfx( Drop_09.11 ~ YellowRed + TotalAcres + large.parcel, data = assessed_bare, atmean = TRUE )
+logit.partial.average.0911
+
+logit.average.partial.0911 <- logitmfx( Drop_09.11 ~ YellowRed + TotalAcres + large.parcel, data = assessed_bare, atmean = FALSE )
+logit.average.partial.0911
+
+#############################################################################
+
+### What happened in 10 -> 11 to parcels that declined in 09 -> 10?
 
 assessed_bare$change.0910.to.1011 <- ( assessed_bare$AssessedLandValue_2011 - assessed_bare$AssessedLandValue_2010 ) / (assessed_bare$AssessedLandValue_2010) 
 assessed_bare$change.0910.to.1011
